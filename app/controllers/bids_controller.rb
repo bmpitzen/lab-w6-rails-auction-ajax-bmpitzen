8d5836b502fc432afecb203789dabe8115cd0776
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-23 20:11:15 -0400

    Unsure of changes

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -3,7 +3,7 @@ class BidsController < ApplicationController
   def create
     @bid = Bid.new(bid_params)
     if @bid.save
-      redirect_to @bid.item, notice: 'Your bid has been recorded!'
+      respond_to :js, @bid.item, notice: 'Your bid has been recorded!'
     else
       @item = @bid.item
       render "items/show"
